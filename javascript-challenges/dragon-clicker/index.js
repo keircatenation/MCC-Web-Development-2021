b6bed3bbb8e4b59e9967b744e735af9e54c6fe4a
Keiran Pillman <keiran.pillman@gmail.com>
2021-07-19 22:34:26 -0400

    changes for Dragon Duo step
    - separated the CSS out into its own file
    - added more styles for layout, color, and typography
    - rehauled the clicking function to make it more scaleable wrt how many dragons can use it and how much it can increment the clicks by
    - added more HTML structure to support the new CSS styling, and also moved the function call to the buttons on the document

--- javascript-challenges/dragon-clicker/index.js
+++ javascript-challenges/dragon-clicker/index.js
@@ -1,8 +1,18 @@
-let dragon = document.querySelector("#dragon");
-let output = document.querySelector("output");
-let value = 0;
+function dragonAddClick(value,element){
+    //creates the selector with the onclick function on the dragon
+    let selector = "." + element + " output";
 
-dragon.addEventListener("click", (e) => {
-    value++;
-    output.innerText = value;
-})
+    //gets the initial value IN THE FORM OF A STRING, then coerced into a number to add
+    let clicks = Number(document.querySelector(selector).innerText);
+
+    //increments with the value passed into the function in the HTML
+    clicks = clicks + value;
+
+    //sets the inner text of the output to hte new value
+    document.querySelector(selector).innerText = clicks;
+}
+
+
+// we want to write code to keep the two clickers separate, showing the # of clicks next to each dragon, and think about how to mae it scalable
+
+// make the global var something that is counted inside????
