73f7688fea8d6bd6dc02667ea8c02ec640e4e087
Keiran Pillman <keiran.pillman@gmail.com>
2021-07-22 20:46:26 -0400

    edited clicks implementation

--- dragon-clicker/index.js
+++ dragon-clicker/index.js
@@ -6,13 +6,10 @@ function dragonAddClick(value,element){
     let clicks = Number(document.querySelector(selector).innerText);
 
     //increments with the value passed into the function in the HTML
-    clicks = clicks + value;
+    clicks += value;
 
     //sets the inner text of the output to hte new value
     document.querySelector(selector).innerText = clicks;
 }
 
 
-// we want to write code to keep the two clickers separate, showing the # of clicks next to each dragon, and think about how to mae it scalable
-
-// make the global var something that is counted inside????
