ed9560563c8f5c3a1351fdde937b8739223819be
Keiran Pillman <keiran.pillman@gmail.com>
2021-07-23 17:45:05 -0400

    dragon arena edits

--- dragon-clicker/index.js
+++ dragon-clicker/index.js
@@ -1,15 +1,30 @@
-function dragonAddClick(value,element){
-    //creates the selector with the onclick function on the dragon
-    let selector = `#${element} output`;
-
-    //gets the initial value IN THE FORM OF A STRING, then coerced into a number to add
-    let clicks = Number(document.querySelector(selector).innerText);
-
-    //increments with the value passed into the function in the HTML
-    clicks += value;
-
-    //sets the inner text of the output to hte new value
-    document.querySelector(selector).innerText = clicks;
+function dragonFactory(elem, color, emoji) {
+    return {
+        element: elem, //becomes the h2 on display, also the ID
+        class: `dragon ${color}`, //styles the div
+        selector: `#${elem} output`,
+        clicks:0, //becomes the output
+        clickFunc: `${elem}Dragon.addClick(1)`,
+        addClick: function(value) {
+            this.clicks += value;
+            document.querySelector(this.selector).innerText = this.clicks;
+        },
+        showDragon: function() {
+            document.querySelector(".arena").innerHTML = "";
+            
+            let dragon = document.createElement("div");
+            dragon.setAttribute("class", this.class);
+            dragon.setAttribute("id", this.element);
+            dragon.setAttribute("onclick", this.clickFunc)
+            dragon.innerHTML = `<h2>${this.element}</h2><p>${emoji}</p><button>🐉</button><output>${this.clicks}</output></div>`;
+            
+            document.querySelector(".arena").appendChild(dragon);
+        }
+    }
 }
 
-
+let fireDragon = dragonFactory("fire", "red", "🔥");
+let waterDragon = dragonFactory("water", "blue", "🌊");
+let earthDragon = dragonFactory("earth", "brown", "🌑");
+let airDragon = dragonFactory("air", "yellow", "💨");
+let heartDragon = dragonFactory("heart", "purple", "💜");
