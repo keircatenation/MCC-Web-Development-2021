d5a97d2eac88660780974ddd6bf2e5b70671293f
Keiran Pillman <keiran.pillman@gmail.com>
2021-07-24 09:46:46 -0400

    editing dragonFactory & CSS
    - changed from createElement to innerHTML for showDragon()
    - edited CSS to use custom variables for colors

--- dragon-clicker/index.js
+++ dragon-clicker/index.js
@@ -1,24 +1,19 @@
 function dragonFactory(elem, color, emoji) {
     return {
-        element: elem, //becomes the h2 on display, also the ID
-        class: `dragon ${color}`, //styles the div
         selector: `#${elem} output`,
-        clicks:0, //becomes the output
-        clickFunc: `${elem}Dragon.addClick(1)`,
+        clicks:0,
         addClick: function(value) {
             this.clicks += value;
             document.querySelector(this.selector).innerText = this.clicks;
         },
         showDragon: function() {
             document.querySelector(".arena").innerHTML = "";
-            
-            let dragon = document.createElement("div");
-            dragon.setAttribute("class", this.class);
-            dragon.setAttribute("id", this.element);
-            dragon.setAttribute("onclick", this.clickFunc)
-            dragon.innerHTML = `<h2>${this.element}</h2><p>${emoji}</p><button>🐉</button><output>${this.clicks}</output></div>`;
-            
-            document.querySelector(".arena").appendChild(dragon);
+            document.querySelector(".arena").innerHTML += `<div class="dragon ${color}" id="${elem}" onclick="${elem}Dragon.addClick(1)">
+                <h2>${elem}</h2>
+                <p>${emoji}</p>
+                <button>🐉</button>
+                <output>${this.clicks}</output>
+            </div>`
         }
     }
 }
