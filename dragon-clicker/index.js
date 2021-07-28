b749e5ddc26157ded6f2b47b586ccdacf67f3f90
Keiran Pillman <keiran.pillman@gmail.com>
2021-07-28 17:58:04 -0400

    took out emoji elements

--- dragon-clicker/index.js
+++ dragon-clicker/index.js
@@ -43,7 +43,7 @@ let dragonSVG = `<?xml version="1.0" encoding="utf-8"?>
 </svg>
 `
 
-function dragonFactory(elem, color, emoji) {
+function dragonFactory(elem, color) {
     return {
         selector: `#${elem} output`,
         clicks:0,
@@ -55,7 +55,6 @@ function dragonFactory(elem, color, emoji) {
             document.querySelector(".arena").innerHTML = "";
             document.querySelector(".arena").innerHTML += `<div class="dragon ${color}" id="${elem}" onclick="${elem}Dragon.addClick(1)">
                 <h2>${elem}</h2>
-                <p>${emoji}</p>
                 <button>${dragonSVG}</button>
                 <output>${this.clicks}</output>
             </div>`
@@ -63,8 +62,8 @@ function dragonFactory(elem, color, emoji) {
     }
 }
 
-let fireDragon = dragonFactory("fire", "red", "🔥");
-let waterDragon = dragonFactory("water", "blue", "🌊");
-let earthDragon = dragonFactory("earth", "brown", "🌑");
-let airDragon = dragonFactory("air", "yellow", "💨");
-let heartDragon = dragonFactory("heart", "purple", "💜");
+let fireDragon = dragonFactory("fire", "red");
+let waterDragon = dragonFactory("water", "blue");
+let earthDragon = dragonFactory("earth", "brown");
+let airDragon = dragonFactory("air", "yellow");
+let heartDragon = dragonFactory("heart", "purple");
