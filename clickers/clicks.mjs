07034c87d337b7e48da491eca95b556a4bd8bc13
Keiran Pillman <keiran.pillman@gmail.com>
2022-12-19 22:27:53 -0500

    clickers

---
+++ clickers/clicks.mjs
@@ -0,0 +1,5 @@
+let clicks = 0;
+
+export function increment() {
+    return clicks++;
+}
---
+++ clickers/main.js
@@ -0,0 +1,53 @@
+import { increment } from "./clicks.mjs";
+
+const closures1 = document.getElementById('closures1')
+const closures2 = document.getElementById('closures2')
+const modules1 = document.getElementById('modules1')
+const modules2 = document.getElementById('modules2')
+const classes1 = document.getElementById('classes1')
+const classes2 = document.getElementById('classes2')
+
+
+// Modules
+modules1.addEventListener( 'click', (e) => {
+    e.target.innerText = increment();
+} )
+modules2.addEventListener( 'click', (e) => {
+    e.target.innerText = increment();
+} )
+
+// Closures
+function updateClicks() {
+    let clicks = 0;
+
+    return function (e) {
+        e.target.innerText = Number(e.target.innerText) + 1
+    }
+}
+const closure1 = updateClicks();
+const closure2 = updateClicks();
+closures1.addEventListener( 'click', e => {
+    closure1(e);
+} )
+closures2.addEventListener( 'click', e => {
+    closure2(e);
+} )
+
+//Classes
+class Clicker{
+    #clicks;
+    #node;
+    constructor(node) {
+        this.#clicks = 0;
+        this.#node = node;
+        this.#node.addEventListener( 'click', () => {
+            this.increment();
+        } )
+    }
+    increment() {
+        this.#clicks ++;
+        this.#node.innerText = this.#clicks;
+    }
+}
+let class1 = new Clicker(classes1)
+let class2 = new Clicker(classes2)
