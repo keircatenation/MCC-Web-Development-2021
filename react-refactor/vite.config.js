e2e4cba3ea66d4b6e70471b317268e683014fc3d
Keiran Pillman <keiran.pillman@gmail.com>
2023-02-16 21:03:11 -0500

    modifying vite config for the dist foler

--- react-refactor/vite.config.js
+++ react-refactor/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/MCC-Web-Development-2021/react-refactor/"
+  base: "/MCC-Web-Development-2021/react-refactor/dist/"
 })
